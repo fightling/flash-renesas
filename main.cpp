c37bd1c3573c9a04e99c6f6ddb653e6a0b7afc59
Patrick Hoffmann <p.hoffmann@berlin.de>
2014-08-15 14:28:02 +0200

    version 0.3

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int main(int _argc, char *_argv[])
   QCoreApplication _a(_argc, _argv);
   {
     _a.setApplicationName("flash-renesas");
-    _a.setApplicationVersion("0.2");
+    _a.setApplicationVersion("0.3");
   }
   // initialize argument parser
   QCommandLineParser _parser;
